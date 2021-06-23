62c012342005a547885ff54ccd3267c2fa6058b9
Nate Stringham <nstringham@wccnet.edu>
2021-06-23 01:31:20 -0400

    refactor: :recycle: move skin viewer to it's own file

--- public/resources/ts/stats-defer.ts
+++ public/resources/ts/stats-defer.ts
@@ -1,5 +1,4 @@
 import { setCookie } from "./common-defer.js";
-import * as skinview3d from "../js/skinview3d.bundle.js";
 
 declare global {
   function tippy(targets: string | Element | Element[], optionalProps?: Record<string, unknown>): any;
@@ -48,63 +47,6 @@ function getCookie(c_name: string) {
 tippy("*[data-tippy-content]:not(.interactive-tooltip)", {
   trigger: "mouseenter click",
 });
-
-const playerModel = document.getElementById("player_model") as HTMLElement;
-
-let skinViewer: any;
-
-if (calculated.skin_data) {
-  skinViewer = new skinview3d.SkinViewer({
-    width: playerModel.offsetWidth,
-    height: playerModel.offsetHeight,
-    model: calculated.skin_data.model,
-    skin: "/texture/" + calculated.skin_data.skinurl.split("/").pop(),
-    cape:
-      calculated.skin_data.capeurl != undefined
-        ? "/texture/" + calculated.skin_data.capeurl.split("/").pop()
-        : "/cape/" + calculated.display_name,
-  });
-
-  playerModel.appendChild(skinViewer.canvas);
-
-  skinViewer.camera.position.set(-18, -3, 58);
-
-  const controls = skinview3d.createOrbitControls(skinViewer);
-
-  skinViewer.canvas.removeAttribute("tabindex");
-
-  controls.enableZoom = false;
-  controls.enablePan = false;
-
-  /**
-   * the average Z rotation of the arms
-   */
-  const basicArmRotationZ = Math.PI * 0.02;
-
-  /**
-   * the average X rotation of the cape
-   */
-  const basicCapeRotationX = Math.PI * 0.06;
-
-  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    skinViewer.animations.add((player: any, time: number) => {
-      // Multiply by animation's natural speed
-      time *= 2;
-
-      // Arm swing
-      const armRotation = Math.cos(time) * 0.03 + basicArmRotationZ;
-      player.skin.leftArm.rotation.z = armRotation;
-      player.skin.rightArm.rotation.z = armRotation * -1;
-
-      // Cape wave
-      player.cape.rotation.x = Math.sin(time) * 0.01 + basicCapeRotationX;
-    });
-  } else {
-    skinViewer.playerObject.skin.leftArm.rotation.z = basicArmRotationZ;
-    skinViewer.playerObject.skin.rightArm.rotation.z = basicArmRotationZ * -1;
-    skinViewer.playerObject.cape.rotation.x = basicCapeRotationX;
-  }
-}
 
 tippy(".interactive-tooltip", {
   trigger: "mouseenter click",
@@ -517,6 +459,8 @@ function closeLore() {
   }
 }
 
+const playerModel = document.getElementById("player_model") as HTMLElement;
+
 let playerModelIsMobile = false;
 
 const navBar = document.querySelector("#nav_bar") as HTMLElement;
@@ -533,14 +477,6 @@ function resize() {
   }
 
   tippy("*[data-tippy-content]");
-
-  if (playerModel && skinViewer) {
-    if (playerModel.offsetWidth / playerModel.offsetHeight < 0.6) {
-      skinViewer.setSize(playerModel.offsetWidth, playerModel.offsetWidth * 2);
-    } else {
-      skinViewer.setSize(playerModel.offsetHeight / 2, playerModel.offsetHeight);
-    }
-  }
 
   navBarHeight = parseFloat(getComputedStyle(navBar).top);
 
